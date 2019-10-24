4f12dec8a8c80b47946489bd7d0a1e1bcd3bdfb1
Efrain Segarra <segarrae@mit.edu>
2019-10-24 11:53:16 -0400

    Updated parameteriizations of minimzier

--- minimizer.cpp
+++ minimizer.cpp
@@ -3,10 +3,12 @@
 #include <cstdlib>
 #include <unistd.h>
 #include <ctype.h>
+#include <Eigen/Dense>
 
 #include "spec.h"
 #include "constants.h"
 
+using Eigen::Vector2d;
 using namespace std;
 
 // Initialize spectral function class
@@ -19,13 +21,21 @@ const double f2nf2p_a = -1.21721713;
 const double f2nf2p_b = 0.8622478;
 const double f2nf2p_c = 0.82047886;
 const double f2nf2p_d = 0.96399233;
+const double f2p_a = 3.07;
+const double f2p_b = 0.75;
+const double f2p_c = 0.14;
+const double f2p_d = -0.19;
+const double f2p_e = -2.93;
+const double f2p_f = -0.05;
+const double f2p_g = 3.65;
 double f2pf2d( double xB , double Q2 );
+double f2p( double xB, double Q2 );
 
 // Main
 int main(int argc, char ** argv){
 
-	if (argc<3){
-		cerr << "Wrong number of arguments used.\n\tPlease instead use: ./gen_eep [nEvents] [outputFile]\n";
+	if (argc<2){
+		cerr << "Wrong number of arguments used.\n\tPlease instead use: ./minimizer [outputFile]\n";
 		return -1;
 	}
 
@@ -34,25 +44,35 @@ int main(int argc, char ** argv){
 	int N = A-Z;
 	
 	// Loop over xB
-	for( double x = 0.2; x < 0.95 ; x+=0.01 ){
+	ofstream outfile; outfile.open(argv[1]);
+	for( double x = 0.2; x <= 1. ; x+=0.01 ){
 	
 		// Spec functions for n/p in He-3
-		std::map<double,std::map<double,double>> test_n = kaptari_sf->getFullN();
-		std::map<double,std::map<double,double>> test_p = kaptari_sf->getFullP();
+		map<double,map<double,double>> test_n = kaptari_sf->getFullN();
+		map<double,map<double,double>> test_p = kaptari_sf->getFullP();
 		// 	iteraotrs to loop over 
-		std::map<double,std::map<double,double>>::iterator itK_n;
-		std::map<double,std::map<double,double>>::iterator itK_p;
-		std::map<double,double>::iterator itE_n;
-		std::map<double,double>::iterator itE_p;
+		map<double,map<double,double>>::iterator itK_n;
+		map<double,map<double,double>>::iterator itK_p;
+		map<double,double>::iterator itE_n;
+		map<double,double>::iterator itE_p;
 		double integral_p = 0;
 		double integral_n = 0;
 		for( itK_n = test_n.begin(), itK_p = test_p.begin(); itK_n != test_n.end() && itK_p!= test_p.end(); itK_n++, itK_p++){
-			double k = itK_n->first/1000.;
+			double p_m = itK_n->first/1000.;
 			for( itE_n = itK_n->second.begin(), itE_p = itK_p->second.begin(); itE_n != itK_n->second.end() && itE_p != itK_p->second.end(); itE_n++, itE_p++){
-				double E = itE_n->first/1000.;
+				double E_m = itE_n->first/1000.;
+
+
+				double E_struck_proton 	= // left-over
+				double E_struck_neutron	= 
+
+
+
 				double v = (k*k + E*E - mN*mN);
 				double sp_n = itE_n->second;
 				double sp_p = itE_p->second;
+				if( Z == 1 ){ // need to swap p/n for He-3 -> H-3 conversion and struck nucleons! TODO
+				}
 
 				integral_p += Z * sp_p * offshell(v,x);
 				integral_n += N * sp_n * offshell(v,x) * f2nf2p(x);
@@ -60,9 +80,10 @@ int main(int argc, char ** argv){
 
 		}
 		
-		cout << (integral_p + integral_n) * f2pf2d(x, 14.*x ) * (2./A) << "\n";
+		outfile << x << " " << (integral_p + integral_n) * f2pf2d(x, 14*x ) * (2./A) << "\n";
 
 	}
+	outfile.close();
 
 	return 0;
 }
@@ -73,34 +94,39 @@ double offshell( double virt, double xB ){
 double f2nf2p( double xB ){
 	return f2nf2p_a + f2nf2p_b*xB + f2nf2p_c*exp( f2nf2p_d*(1.-xB) );
 }
-double f2pf2d( double xB , double Q2 ){
-	double C1  [2] = { 1.417, 0.948}; 
-	double C2  [2] = {-0.108,-0.115}; 
-	double C3  [2] = { 1.486, 1.861}; 
-	double C4  [2] = {-5.979,-4.733}; 
-	double C5  [2] = { 3.524, 2.348}; 
-	double C6  [2] = {-0.011,-0.065}; 
-	double C7  [2] = {-0.619,-0.224}; 
-	double C8  [2] = { 1.385, 1.085}; 
-	double C9  [2] = { 0.270, 0.213}; 
-	double C10 [2] = {-2.179,-1.687}; 
-	double C11 [2] = { 4.722, 3.409}; 
-	double C12 [2] = {-4.363,-3.255}; 
-	double beta [2] = { 1.	, pow( 1. - exp( -min(20.,7.7*(1./xB + mP*mP/Q2 - 1. ) ) ) , -1 ) };
-	double A = 1.22*exp(3.2*xB);
-	double lambda2[2] = C6 + C7*xB + C8*xB*xB;
 
-	/*double lambda2[2] = C6 + C7*x + C8*x*x
-
-	lambda1 = C9 + C10*x + C11*x*x + C12*x*x*x
-
-	F2_thr = C1*np.power(1.-x,3) + C2*np.power(1.-x,4) + C3*np.power(1.-x,5) + C4*np.power(1.-x,6) + C5*np.power(1.-x,7)
-	
-	if( Q2 > A ): lambda2 = [0.,0.]
-	mult0 = 1. + lambda1[0]*np.log(Q2/A) + lambda2[0]*np.power( np.log(Q2/A) , 2 )
-	mult1 = 1. + lambda1[1]*np.log(Q2/A) + lambda2[1]*np.power( np.log(Q2/A) , 2 )
-	return (beta[0]*F2_thr[0]*mult0)/(beta[1]*F2_thr[1]*mult1)/2.*/
-	return 1.;
-
+double f2p( double xB, double Q2 ){
+	return (f2p_a*pow(x,f2p_b) + f2p_c*pow(x,f2p_d)*(1.+f2p_e*sqrt(x))*(log(Q2)+f2p_f*pow(log(Q2),2))) * pow(1.-x,f2p_g);
 }
 
+
+
+double f2pf2d( double xB , double Q2 ){
+	// Using Whitlow parameterization for the moment
+	Vector2d C1( 1.417, 0.948); 
+	Vector2d C2(-0.108,-0.115); 
+	Vector2d C3( 1.486, 1.861); 
+	Vector2d C4(-5.979,-4.733); 
+	Vector2d C5( 3.524, 2.348); 
+	Vector2d C6(-0.011,-0.065); 
+	Vector2d C7(-0.619,-0.224); 
+	Vector2d C8( 1.385, 1.085); 
+	Vector2d C9( 0.270, 0.213); 
+	Vector2d C10(-2.179,-1.687); 
+	Vector2d C11( 4.722, 3.409); 
+	Vector2d C12(-4.363,-3.255); 
+
+	Vector2d beta(1., pow( 1. - exp( -min(20.,7.7*(1./xB + mP*mP/Q2 - 1. ) ) ) , -1 ) );
+	double A = 1.22*exp(3.2*xB);
+
+	Vector2d lambda1, lambda2, F2_thr;
+	lambda2 = C6 + C7*xB + C8*xB*xB;
+	lambda1 = C9 + C10*xB + C11*xB*xB + C12*xB*xB*xB;
+	F2_thr = C1*pow(1.-xB,3) + C2*pow(1.-xB,4) + C3*pow(1.-xB,5) + C4*pow(1.-xB,6) + C5*pow(1.-xB,7);
+
+	if( Q2 > A ) lambda2(0.,0.);
+	double mult0 = 1. + lambda1[0]*log(Q2/A) + lambda2[0]*pow( log(Q2/A) , 2 );
+	double mult1 = 1. + lambda1[1]*log(Q2/A) + lambda2[1]*pow( log(Q2/A) , 2 );
+
+	return (beta[0]*F2_thr[0]*mult0)/(beta[1]*F2_thr[1]*mult1)/2.;
+}
